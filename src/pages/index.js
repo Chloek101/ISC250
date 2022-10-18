56f555799a8ea0ccf08caf8d8b51df6fcc26e475
Chloek101 <chloerk@email.sc.edu>
2022-10-18 14:22:08 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
 import * as React from "react"
 import { Link } from 'gatsby'
+import "../components/index.css"
+
 
 const Index = () => {
   return (
-    <div>
-      <Link to="/about">About Page</Link>
-      <h1>My Landing Page</h1>
-      <p>This is my landing page</p>
-    </div>
+    <header className= 'hero'>
+      <div className= 'heroContainer'>
+        <div className= 'heroInfo'>
+        <h1> Chloe King</h1>
+        <p>Infomration Design</p>
+        <Link to="/about">See how we can help</Link>
+        </div>
+      </div>
+    </header>
   )
 }
-export default Index
+
+export default index; 
