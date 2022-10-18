9d3d057da4471f04e018f9ac1f49c4d11b8b26df
Chloek101 <chloerk@email.sc.edu>
2022-10-18 14:03:34 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const About = () => {
     return (
         <div>
+            <Link to="/">Home Page</Link>
             <h1>My About Page</h1>
             <p>This is my about page</p>
         </div>
